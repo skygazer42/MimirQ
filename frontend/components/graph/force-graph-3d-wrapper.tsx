54ba6f08e713d01b0107d77cd5928b65ca6901ff
jhlucc <103022245+jhlucc@users.noreply.github.com>
2026-01-03 11:06:28 +0800

    删除3d 前端可视化

--- frontend/components/graph/force-graph-3d-wrapper.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-import ForceGraph3D from 'react-force-graph-3d'
-
-const ForceGraph3DWrapper = ({ graphRef, ...props }: any) => {
-  return <ForceGraph3D ref={graphRef} {...props} />
-}
-
-export default ForceGraph3DWrapper
