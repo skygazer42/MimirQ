27393d418b07c3e22883bfc8f092b232c841ee96
skygazer42 <207829897@qq.com>
2026-01-27 09:09:58 +0800

    Remove temporary SQLAlchemy check script
    - Deleted the .tmp_sqlalchemy_check.py file, which contained unused imports and code for testing SQLAlchemy functionality.
    - This cleanup helps maintain a cleaner codebase by removing unnecessary files.

--- .tmp_sqlalchemy_check.py
+++
@@ -1,7 +0,0 @@
-import sqlalchemy
-import sqlalchemy as sa
-from sqlalchemy.dialects.postgresql import JSONB
-col = sa.column(" x\, JSONB)
-expr = col[\a\]
-print(sqlalchemy.__version__)
-print([m for m in dir(expr) if m.startswith(\as_\)])
