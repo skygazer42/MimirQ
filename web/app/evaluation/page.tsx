6be95dca52764b92f406fdec65bfc3ef48e7c9f3
skygazer42 <103022245+skygazer42@users.noreply.github.com>
2026-03-30 16:24:24 +0800

    Extend next-intl static route coverage

--- web/app/evaluation/page.tsx
+++ web/app/evaluation/page.tsx
@@ -1,5 +1,6 @@
-import { redirect } from 'next/navigation'
+import { redirect } from '@/i18n/navigation'
+import { routing } from '@/i18n/routing'
 
 export default function EvaluationRedirectPage() {
-  redirect('/evaluations')
+  redirect({ href: '/evaluations', locale: routing.defaultLocale })
 }
